1ef79dccf9e376415bcdd860c91d03a810d321ab
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-06-25 16:48:35 +0200

    fixed card style

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -4,11 +4,16 @@ import libriHorror from "../data/horror.json";
 const AllTheBooks = () => {
   return (
     <Container fluid="md">
+      <h1 className="my-4">HORROR</h1>
       <Row>
         {libriHorror.map((book) => (
           <Col lg={3} md={6} sm={12} key={book.asin} className="mb-4">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={book.img} />
+            <Card className="cardStyle">
+              <Card.Img
+                className="rounded img-cover"
+                variant="top"
+                src={book.img}
+              />
               <Card.Body>
                 <Card.Title>{book.title}</Card.Title>
                 <Card.Text>{"prezzo " + book.price + "€"}</Card.Text>
