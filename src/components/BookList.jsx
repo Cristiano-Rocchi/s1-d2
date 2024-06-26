ef3f8da8cba294e5e525727d6b3cb719ce1d875f
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-06-26 16:43:24 +0200

    esercizio 2 e 3

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,26 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import SingleBook from "./SingleBook";
+
+class BookList extends React.Component {
+  render() {
+    console.log("props", this.props);
+    const { books } = this.props;
+
+    return (
+      <Row>
+        {books.map((book) => (
+          <Col lg={3} md={6} sm={12} key={book.asin} className="mb-4">
+            <SingleBook book={book} />
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+}
+
+export default BookList;
+/* import { Col, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 const BookList = ({ books }) => {
   return (
@@ -12,4 +33,4 @@ const BookList = ({ books }) => {
     </Row>
   );
 };
-export default BookList;
+export default BookList; */
