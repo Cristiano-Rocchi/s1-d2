2521dcd2c3bdd0b4ed0b87829699feb966132599
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-06-26 17:02:44 +0200

    tutti gli esercizi

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const SingleBook = ({ book }) => {
-  return (
-    <Card className="mb-4">
-      <Card.Img variant="top" src={book.img} className="rounded img-cover" />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  );
-};
+class SingleBook extends React.Component {
+  state = {
+    selected: false,
+  };
+
+  toggleSelected = () => {
+    this.setState((prevState) => ({
+      selected: !prevState.selected,
+    }));
+  };
+
+  render() {
+    const { book } = this.props;
+    const { selected } = this.state;
+
+    return (
+      <Card className={`mb-4 ${selected ? "border border-danger" : ""}`}>
+        <Card.Img
+          variant="top"
+          src={book.img}
+          className="rounded img-cover"
+          onClick={this.toggleSelected}
+          style={{ cursor: "pointer" }}
+        />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default SingleBook;
